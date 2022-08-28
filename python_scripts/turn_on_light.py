095e0c6424b14ec1510e250b8ec486fa30704f6b
Ian <utiancarter@gmail.com>
2022-08-28 22:03:47 +0800

    Removed dictionary from turn_on_light script

--- python_scripts/turn_on_light.py
+++ python_scripts/turn_on_light.py
@@ -1,11 +1,9 @@
-light = dict()
+warm_light_mireds = 500
+cool_white_mireds = 326
+daylight_mireds = 153
 
-light["warm_light_mireds"] = 500
-light["cool_white_mireds"] = 326
-light["daylight_mireds"] = 153
-
-light['min_brightness_pct'] = 9
-light['max_brightness_pct'] = 100
+min_brightness_pct = 9
+max_brightness_pct = 100
 
 sleeptime_brightness_pct = 20
 nighttime_brightness_pct = 60
@@ -13,20 +11,20 @@ daytime_brightness_pct = 100
 
 entity_id = data.get("entity_id")
 
-transition = 1
+# transition = 1
 
 if datetime.datetime.now().hour < 6:
     brightness = sleeptime_brightness_pct
-    color_temp = light["warm_light_mireds"]
+    color_temp = warm_light_mireds
 elif datetime.datetime.now().hour > 20:
     brightness = nighttime_brightness_pct
-    color_temp = light["warm_light_mireds"]
+    color_temp = warm_light_mireds
 else:
     brightness = daytime_brightness_pct
-    color_temp = light["warm_light_mireds"]
+    color_temp = warm_light_mireds
 
 if hass.states.get('weather.forecast_207a_home') in ['Rainy', 'Cloudy']:
-    color_temp = light["daylight_mireds"]
+    color_temp = daylight_mireds
 
 if entity_id is not None:
     service_data = {"entity_id": entity_id, "color_temp": color_temp, "brightness_pct": brightness}
