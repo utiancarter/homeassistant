512b44e719789b36eae138615c41a2ad51afd96a
Ian Carter <ian@personalise.gov.sg>
2022-09-06 19:49:11 +0800

    added criteria to limit lights to adjust

--- python_scripts/sunset_change_light_temp.py
+++ python_scripts/sunset_change_light_temp.py
@@ -9,14 +9,18 @@ sleeptime_brightness_pct = 20
 nighttime_brightness_pct = 60
 daytime_brightness_pct = 100
 
-lights_on = [light for light in hass.states.entity_ids(domain_filter = 'light') if hass.states.get(light).state == 'on']
+# Get list of entity_ids whose state is "on", not a group, and color_mode set to color_temp
+lights_on = [light for light in hass.states.entity_ids(domain_filter = 'light') if ((hass.states.get(light).state == 'on') & ('icon' not in hass.states.get(light).keys()) & (hass.states.get(light).color_mode != 'color_temp'))]
 
-logger.info(f'The sun is setting! These lights were found on: {", ".join(lights_on)}')
+logger.info(f'The sun is setting! These lights were found on and set to color_temp: {", ".join(lights_on)}')
 
-for light in lights_on:
+def adjust_light(light, mireds, brightness_pct):
     service_data = {
         "entity_id": light, 
-        "color_temp": daylight_mireds, 
-        "brightness_pct": nighttime_brightness_pct
+        "color_temp": mireds, 
+        "brightness_pct": brightness_pct
         }
     hass.services.call("light", "turn_on", service_data, False)
+
+for light in lights_on:
+    adjust_light(light, daylight_mireds, nighttime_brightness_pct)
